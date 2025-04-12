82ce0f3ba9d00058c3fcb79a6336eada4c1ed561
Lucas Duarte <luckasdu686@gmail.com>
2025-04-12 13:23:05 -0300

    iniciando desenvolvimento

---
+++ README.md
@@ -0,0 +1,48 @@
+# Hungry Burger - Seu site de pedidos
+
+### Acesse o Site:
+
+Visite o [Site aqui!](https://pedidoburger.vercel.app/)
+
+Este projeto consiste em um site de pedidos online para uma lanchonete fictícia chamada Hungry Burger. Os clientes podem visualizar o menu, adicionar itens ao carrinho e finalizar seus pedidos. O site também fornece informações sobre o horário de funcionamento e a localização da lanchonete.
+
+## Como utilizar
+
+1. **Visualizando o Menu**: Na página inicial, você encontrará uma variedade de itens no menu, incluindo hambúrgueres e bebidas. Cada item exibe uma imagem, nome, descrição e preço.
+
+2. **Adicionando ao Carrinho**: Ao clicar no botão "Adicionar ao Carrinho" abaixo de cada item, o produto será adicionado ao seu carrinho de compras.
+
+3. **Visualizando o Carrinho**: Clique no botão "Veja meu carrinho" no rodapé para visualizar os itens adicionados ao carrinho. Você pode aumentar ou diminuir a quantidade de cada item, bem como remover itens individualmente.
+
+4. **Finalizando o Pedido**: Após revisar seu pedido, preencha seu endereço de entrega e selecione o método de pagamento. Se optar por pagamento em dinheiro, insira o valor para o troco. Em seguida, clique em "Finalizar Pedido" para enviar seu pedido para a lanchonete via WhatsApp.
+
+5. **Horário de Funcionamento**: O site exibe o horário de funcionamento da lanchonete, indicando se está aberta ou fechada no momento.
+
+## Stacks utilizadas
+
+**Front-end:** HTML5, CSS3, TailwindCSS
+
+**Back-end:** jQuery, JavaScript
+
+## Recursos Externos
+
+- Font Awesome: Utilizado para ícones.
+- Google Fonts: Fonte utilizada no site.
+- Toastify: Biblioteca para exibir alertas personalizados.
+- jQuery: Biblioteca JavaScript para manipulação do DOM.
+- jQuery Mask: Plugin jQuery para máscaras em campos de input.
+
+## Observações
+
+- O site possui integração com o WhatsApp para finalização dos pedidos.
+- O horário de funcionamento é dinâmico e indica se a lanchonete está aberta ou fechada com base no horário atual.
+
+O horário pode ser alterado no seguinte trecho do arquivo: [script.js](https://github.com/LuckasDuarte/BURGER_SITE/blob/main/script.js) linha 271
+
+![Codigo](https://github.com/LuckasDuarte/BURGER_SITE/blob/main/assets/JS_HORA.png)
+
+
+---
+
+Este projeto foi desenvolvido por Lucas Duarte como parte de um exercício de prática de desenvolvimento web. Para mais informações, entre em contato através do linkedin: [Perfil Linkedin](https://www.linkedin.com/in/lucas-duarte-batista-7a5a2b218/)
+
---
+++ script.js
@@ -0,0 +1,295 @@
+// Obtendo elementos a serem manipulados
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItensContainer = document.getElementById("cart-itens")
+const cartTotal = document.getElementById("cart-total")
+const checkOutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCount = document.getElementById("cart-count")
+const adressInput = document.getElementById("adress")
+const adressWarning = document.getElementById("adress-warn")
+
+
+// Obtenha a referência para o seletor de pagamento e o campo de troco
+const paymentMethodSelect = document.getElementById("payment-method");
+const trocoSection = document.getElementById("troco-section");
+const trocoValue = document.getElementById("troco");
+
+
+// Adicione um ouvinte de evento para o seletor de pagamento
+paymentMethodSelect.addEventListener("change", function() {
+    const selectedOption = paymentMethodSelect.value;
+    if (selectedOption === "Dinheiro") {
+        // Se a opção selecionada for "dinheiro", mostre o campo de troco
+        trocoSection.style.display = "block";
+    } else {
+        // Caso contrário, oculte o campo de troco
+        trocoSection.style.display = "none";
+    }
+});
+
+
+let cart = [];
+
+
+// Abrindo Modal
+cartBtn.addEventListener("click", function() {
+    updateCartModal()
+
+    cartModal.style.display = "flex"
+})
+
+// Fechando com click fora
+cartModal.addEventListener("click", function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+// Fechando Modal
+closeModalBtn.addEventListener("click", function(){
+    cartModal.style.display = "none"
+})
+
+// Obtendo o Produto Selecionado
+menu.addEventListener("click", function(event){
+
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        
+        // Adicionando no Carrinho
+        addToCart(name, price)
+    }
+})
+
+// Adicionando no Carrrinho
+function addToCart(name, price){
+
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+        // Se o item ja existe no carrinho aumenta a quantidade + 1
+        existingItem.qtd += 1
+
+        Toastify({
+            text: `${name} foi adicionado no carrinho!`,
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#005b32",
+            },
+        }).showToast();
+
+    } else{
+        cart.push({
+            name,
+            price,
+            qtd: 1,
+        });
+
+        Toastify({
+            text: `${name} foi adicionado no carrinho!`,
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#005b32",
+            },
+        }).showToast();
+    }
+
+    updateCartModal()
+}
+
+// Atualiza o Carrinho
+function updateCartModal(){
+    cartItensContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item =>{
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex","justify-between","mb-4","flex-col")
+
+        cartItemElement.innerHTML = `
+            <div class="flex items-center justify-between">
+                <div>   
+                    <p class="font-bold ">${item.name}</p>
+                    <p>Quantidade: ${item.qtd}</p>
+                    <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+                </div>
+                <button class="remove-btn mx-1" data-name="${item.name}">
+                    Remover
+                </button>
+            </div>
+        `
+        total += item.price * item.qtd
+
+
+        cartItensContainer.appendChild(cartItemElement);
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
+    cartCount.innerText = cart.length;
+}
+
+// Removendo Item do Carrinho
+cartItensContainer.addEventListener("click", function(event){
+
+    if(event.target.classList.contains("remove-btn")){
+        const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name);
+        Toastify({
+            text: `${name} foi removido do carrinho!`,
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#ef4444",
+            },
+        }).showToast();
+
+    }
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name)
+
+    if(index !== -1){
+        const item = cart[index];
+
+        if(item.qtd > 1){
+            item.qtd -= 1;
+            updateCartModal()
+            return;
+        }
+
+        cart.splice(index, 1)
+        updateCartModal()
+    }
+}
+
+adressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        adressInput.classList.remove("border-red-500")
+        adressWarning.classList.add("hidden")
+    }
+
+})
+
+
+// Finalizar Pedido
+checkOutBtn.addEventListener("click", function(){
+
+    const isOpen = checkOpen()
+    if(!isOpen){
+
+        Toastify({
+            text: "Estamos Fechados no momento!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#ef4444",
+            },
+        }).showToast();
+
+        return;
+    }
+
+    if(cart.length === 0) return;
+
+    if(adressInput.value === ""){
+        adressWarning.classList.remove("hidden")
+        adressInput.classList.add("border-red-500")
+        return;
+    }
+
+    // Calcula o total dos itens no carrinho
+    let total = 0;
+    cart.forEach(item => {
+        total += item.price * item.qtd;
+    });
+
+    // enviar via whatsapp
+    const cartItens = cart.map((item) => {
+        return(
+`${item.name}
+Quantidade: ( ${item.qtd} )
+Preço: R$ ${item.price.toFixed(2)}\n\n`
+            )
+    }).join("")
+
+    const valorSelecionado = paymentMethodSelect.options[paymentMethodSelect.selectedIndex].value;
+
+    if(valorSelecionado === "Dinheiro"){
+
+        
+
+        const message = encodeURI(`Olá! Gostaria de fazer um pedido:\n\n${cartItens}Endereço: ${adressInput.value}\nTotal: R$ ${total.toFixed(2)}\nForma de Pagamento: *${valorSelecionado}*\nValor Pagamento: *${trocoValue.value}*`)
+
+        const phone = "12996860694"
+
+        window.open(`https://wa.me/${phone}?text=${message}`,"_blank")
+    }else{
+        const message = encodeURI(`Olá! Gostaria de fazer um pedido:\n\n${cartItens}Endereço: ${adressInput.value}\nTotal: R$ ${total.toFixed(2)}\nForma de Pagamento: *${valorSelecionado}*`)
+
+        const phone = "12996860694"
+    
+        window.open(`https://wa.me/${phone}?text=${message}`,"_blank")
+    }
+    cart = []
+    trocoValue.value = ""
+    adressInput.value = ""
+    cartModal.style.display = "none"
+
+    updateCartModal()
+    
+})
+
+
+// Verificando Horario 
+function checkOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+  
+    return hora >= 6 && hora < 20; // True = aberto
+}
+
+const spanDate = document.getElementById("date-span")
+const isOpen = checkOpen();
+
+if(isOpen === true){
+    spanDate.classList.remove("bg-red-500")
+    spanDate.classList.add("bg-green-500")
+} else {
+    spanDate.classList.remove("bg-green-500")
+    spanDate.classList.add("bg-red-500")
+    
+}
+
+
+
+
+
+
+
